05478fc4d7c76e581ded8e6b4e30e57fd46bc2da
imyasergei <69170674+imyasergei@users.noreply.github.com>
2020-09-08 23:16:42 +0300

    Update SerhiiVakulenkoPythonScript.py

--- SerhiiVakulenkoPythonScript.py
+++ SerhiiVakulenkoPythonScript.py
@@ -20,25 +20,26 @@ url = "https://sandbox.tinypass.com/api/v3/publisher/user/list?aid=o1sRRZSLlw&ap
 html_content = requests.get(url).text
 response2 = BeautifulSoup(html_content, "lxml")
 response = response2.body.text
-parsed = json.loads(response)
-usr_data = parsed["users"]
-users_data = open('C://test2.csv', 'w')
-csvwriter = csv.writer(users_data)
+response3 = json.loads(response)
+employee_parsed = json.loads(response)
+emp_data = employee_parsed["users"]
+employ_data = open('C://API_users.csv', 'w')
+csvwriter = csv.writer(employ_data)
 count = 0
-for user in usr_data:
+for emp in emp_data:
       if count == 0:
              header = emp.keys()
              csvwriter.writerow(header)
              count += 1
-      csvwriter.writerow(user.values())
+      csvwriter.writerow(emp.values())
 employ_data.close()
 
-#MERGE FINAL
-c = pd.read_csv("C://test2.csv")
+#MERGE FileA,FileB,API_users
+c = pd.read_csv("C://API_users.csv")
 c.columns = ["first_name","last_name","personal_name","email","user_id","image1","create_date","reset_password_email_sent","custom_fields"]
 c2 = c [["user_id","email","first_name","last_name"]]
 left_merged = merged.merge(c2, how="left", on="email")
 left_merged.rename(columns={'first_name_x':'first_name','last_name_x':'last_name','user_id_y':'user_id'},inplace=True)
+left_merged.user_id.fillna(left_merged.user_id_x, inplace=True)
 left_merged2 = left_merged[["email","first_name","last_name","user_id"]]
-left_merged2 = left_merged2.merge(c2, how="left", on="first_name")
-left_merged2.to_csv("C://output.csv", index=False) #FINAL FILE C://output.csv
+left_merged2.to_csv("C://output.csv", index=False) #FINAL MERGED FILE C://output.csv
